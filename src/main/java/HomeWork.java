88f154990332de04a6084488ad451fe07b849154
Aqilalh <Aqilalhaidari@gmail.com>
2022-08-31 16:51:48 -0400

    hey

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -1,5 +1,10 @@
 public class HomeWork {
     public static void main(String[] args) {
         System.out.println("what it do");
+        //java output
+        System.out.println("How we doing");
+        System.out.println("I am practicing java");
+        System.out.println(4 + 3);
+        // system.out.println helps us to out put values and to print text
     }
 }
