88b9f6a67439067948e99087bc538ef38fe7c9e7
Aqilalh <Aqilalhaidari@gmail.com>
2022-08-31 20:18:25 -0400

    hey

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -6,5 +6,39 @@ public class HomeWork {
         System.out.println("I am practicing java");
         System.out.println(4 + 3);
         // system.out.println helps us to out put values and to print text
+        // Java variables
+        String name = "Aqil";
+        System.out.println(name);
+        int myNum = 17;
+        System.out.println(myNum);
+        int myNumb;
+        myNumb = 21;
+        System.out.println(myNumb);
+        int myNumber = 6;
+        float myFloatNum = 22f;
+        String myLetter = "D";
+        boolean myBool = true;
+        String myText = "How you doing";
+        String carName = "volvo";
+        System.out.println(carName);
+        String firstName = "Aqil";
+        String lastName = "Alhaidari";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        int x = 10;
+        int y = 15;
+        System.out.println(x + y);
+
+        int f = 5, g = 6, z = 50;
+        System.out.println(f + g + z);
+
+        int l, j, h;
+        l =20;
+        j=25;
+        l = j = h = 50;
+        System.out.println(l + g + h);
+        // Data types
+
+
     }
 }
